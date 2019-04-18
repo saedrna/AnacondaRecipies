f91d3045aed965c8dc710156171b265ee24b6f75
Han Hu <huhan@whu.edu.cn>
2019-04-18 19:28:38 +0800

    加入 viennacl

---
+++ viennacl/bld.bat
@@ -0,0 +1,24 @@
+mkdir build
+cd build
+
+:: CMake/OpenCV like Unix-style paths for some reason.
+set UNIX_PREFIX=%PREFIX:\=/%
+set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
+set UNIX_LIBRARY_INC=%LIBRARY_INC:\=/%
+set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
+set UNIX_SP_DIR=%SP_DIR:\=/%
+set UNIX_SRC_DIR=%SRC_DIR:\=/%
+
+cmake -G "Ninja" ^
+    -DBUILD_TESTING=OFF ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX% ^
+    ../
+
+if errorlevel 1 exit 1
+cmake --build . --target install --config Release
+
+exit 0
