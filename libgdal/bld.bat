5ceb674ed9a6d63a40e40ab2993a15aa649cd6c8
Han Hu <huhan@whu.edu.cn>
2019-04-18 12:30:29 +0800

    加入 gdal

---
+++ libgdal/bld.bat
@@ -0,0 +1,29 @@
+@echo ON
+setlocal enabledelayedexpansion
+
+set CL=/MP
+
+nmake /f makefile.vc WIN64=YES MSVC_VER=1910 ^
+    GDAL_HOME=%LIBRARY_PREFIX% ^
+    BINDIR=%LIBRARY_BIN% ^
+    LIBDIR=%LIBRARY_LIB% ^
+    INCDIR=%LIBRARY_INC% ^
+    DATADIR=%LIBRARY_PREFIX%\share\gdal ^
+    HTMLDIR=%LIBRARY_PREFIX%\share\doc\gdal 
+
+if errorlevel 1 exit 1
+
+mkdir -p %LIBRARY_PREFIX%\share\doc\gdal
+
+nmake /f makefile.vc devinstall WIN64=YES MSVC_VER=1910  ^
+    GDAL_HOME=%LIBRARY_PREFIX% ^
+    BINDIR=%LIBRARY_BIN% ^
+    LIBDIR=%LIBRARY_LIB% ^
+    INCDIR=%LIBRARY_INC% ^
+    DATADIR=%LIBRARY_PREFIX%\share\gdal ^
+    HTMLDIR=%LIBRARY_PREFIX%\share\doc\gdal 
+
+if errorlevel 1 exit 1
+
+copy *.lib %LIBRARY_LIB%\ || exit 1
+if errorlevel 1 exit 1
