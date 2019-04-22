e15a5b5ce0dc4fb45fcc947a059fdeccdd547878
Han <huhan8807@gmail.com>
2019-04-22 16:30:35 +0800

    opencl linux

--- opencl/build.sh
+++ opencl/build.sh
@@ -3,9 +3,8 @@ sed -i.bak  "s|/etc/OpenCL/vendors/|$PREFIX/etc/OpenCL/vendors/|g" icd_linux.c;
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make -j${CPU_COUNT}
-make install
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --target install --config Release
 
 if [[ "$(uname)" == "Darwin" ]]; then
     ln -s $PREFIX/include/CL $PREFIX/include/OpenCL
