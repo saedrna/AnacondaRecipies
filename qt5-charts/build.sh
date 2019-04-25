1cf1a739dcfce595c55527f0f29cec37fe3de5db
Han Hu <huhan@whu.edu.cn>
2019-04-25 18:19:47 +0800

    修改编译器

--- qt5-charts/build.sh
+++ qt5-charts/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Remove the full path from CXX etc. If we don't do this
+# then the full path at build time gets put into 
+# mkspecs/qmodule.pri and qmake attempts to use this.
+export AR=$(basename ${AR})
+export RANLIB=$(basename ${RANLIB})
+export STRIP=$(basename ${STRIP})
+export OBJDUMP=$(basename ${OBJDUMP})
+export CC=$(basename ${CC})
+export CXX=$(basename ${CXX})
+
 qmake CONFIG+=release
 make all -j$CPU_COUNT
 make install
