584d960d366fe34b4ac99f9edf835b029653b5a8
Han Hu <huhan@whu.edu.cn>
2019-04-25 12:28:19 +0800

    laszip windows

---
+++ laszip/bld.bat
@@ -0,0 +1,24 @@
+mkdir build-vs
+cd build-vs
+
+:: CMake/OpenCV like Unix-style paths for some reason.
+set UNIX_PREFIX=%PREFIX:\=/%
+set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
+set UNIX_LIBRARY_INC=%LIBRARY_INC:\=/%
+set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
+set UNIX_SP_DIR=%SP_DIR:\=/%
+set UNIX_SRC_DIR=%SRC_DIR:\=/%
+
+cmake -G "Ninja" ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%UNIX_LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX% ^
+    -DLASZIP_BUILD_STATIC=OFF ^
+    ../
+
+if errorlevel 1 exit 1
+cmake --build . --target install --config Release
+
+exit 0
---
+++ laszip/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $(uname) == Darwin ]]; then
+  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+elif [[ $(uname) == Linux ]]; then
+  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
+
+mkdir build_gcc
+cd build_gcc
+
+cmake -G "Ninja" \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_SYSROOT=$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot \
+      -DLASZIP_BUILD_STATIC=OFF \
+      ../
+
+cmake --build . --target install --config Release
