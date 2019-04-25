72efbbb64bbdbc14ade5397c15913fa1978685f4
Han <huhan8807@gmail.com>
2019-04-25 22:01:36 +0800

    imageformat 差 zlib

--- build_all.sh
+++ build_all.sh
@@ -4,37 +4,38 @@ sudo apt install libgl1-mesa-dev \
 
 conda config --add channels conda-forge
 conda config --add channels saedrna
+conda config --set anaconda_upload yes
 
 conda activate base
 
-# conda-build assimp
-# conda-build boost-cpp
-# conda-build capstone
-# conda-build catch2
-# conda-build cereal
-# conda-build cgal-cpp
-# conda-build flann
-# conda-build geographiclib
-# conda-build glog
-# conda-build khronos-opencl-icd-loader
-# conda-build laszip
-# conda-build libgdal
-# conda-build libtiff
-# conda-build qhull
-# conda-build suitesparse
+conda-build assimp
+conda-build boost-cpp
+conda-build capstone
+conda-build catch2
+conda-build cereal
+conda-build cgal-cpp
+conda-build flann
+conda-build geographiclib
+conda-build glog
+conda-build khronos-opencl-icd-loader
+conda-build laszip
+conda-build libgdal
+conda-build libtiff
+conda-build qhull
+conda-build suitesparse
 
-# conda-build viennacl
+conda-build viennacl
 
-# conda-build coinutils
-# conda-build osi
-# conda-build clp
-# conda-build openmvg
+conda-build coinutils
+conda-build osi
+conda-build clp
+conda-build openmvg
 
-# conda-build pcl
+conda-build pcl
 
-# conda-build open3d
+conda-build open3d
 
-# conda-build ceres-solver
+conda-build ceres-solver
 
 conda-build qt5-base
 conda-build qt5-declarative
