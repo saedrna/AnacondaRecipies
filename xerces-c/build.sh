1c98e9dc4d1fc06bf4993ebe878f83498d44a2a9
Han <huhan8807@gmail.com>
2019-04-27 18:15:37 +0800

    linux xerces-c

--- xerces-c/build.sh
+++ xerces-c/build.sh
@@ -3,6 +3,8 @@ mkdir build_gcc
 cd build_gcc
 
 cmake -GNinja \
+    -DCMAKE_AR=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar \
+    -DCMAKE_SYSROOT=$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot \
     -DCMAKE_BUILD_TYPE=Release              \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
