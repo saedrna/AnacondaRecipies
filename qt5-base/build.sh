e9c7c347d934afde11a37b7b03f2b994d07e26b6
han <huhan8807@gmail.com>
2019-05-23 23:46:49 +0800

    1

--- qt5-base/build.sh
+++ qt5-base/build.sh
@@ -9,8 +9,8 @@ do
     unset $x
 done
 
-ln -s ${GXX} $BUILD_PREFIX/bin/g++ || true
-ln -s ${GCC} $BUILD_PREFIX/bin/gcc || true
+ln -s $BUILD_PREFIX/bin/${HOST}-g++ $BUILD_PREFIX/bin/g++ || true
+ln -s $BUILD_PREFIX/bin/${HOST}-gcc $BUILD_PREFIX/bin/gcc || true
 ln -s $BUILD_PREFIX/bin/${HOST}-gcc-ar $BUILD_PREFIX/bin/gcc-ar || true
 
 # Horrible hack! We should not `conda install` in build.sh!!!
@@ -23,16 +23,27 @@ export OPENSSL_LIBS="-L${SRC_DIR}/openssl_hack/lib -lssl -lcrypto"
 
 rm -rf ${PREFIX}/include/openssl
 
+declare -a INCDIRS
+INCDIRS=(-I ${PREFIX}/include)
+SYSINCDIRS=$(echo "" | ${CXX} -xc++ -E -v - 2>&1 | awk '/#include <...> search starts here:/{flag=1;next}/End of search list./{flag=0}flag')
+for SYSINCDIR in ${SYSINCDIRS}; do
+    INCDIRS+=(-I ${SYSINCDIR})
+done
+
 ./configure -prefix $PREFIX \
+            -extprefix $PREFIX \
+            -hostprefix $PREFIX \
             -libdir $PREFIX/lib \
             -bindir $PREFIX/bin \
             -headerdir $PREFIX/include/qt \
             -archdatadir $PREFIX \
             -datadir $PREFIX \
-            -L $PREFIX/lib \
-            -L $BUILD_PREFIX/$HOST/sysroot/usr/lib \
-            -I $PREFIX/include \
-            -I ${SRC_DIR}/openssl_hack/include \
+            -sysroot ${BUILD_PREFIX}/${HOST}/sysroot \
+            -L$PREFIX/lib \
+            -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 \
+            -I$PREFIX/include \
+            -I${SRC_DIR}/openssl_hack/include \
+            "${INCDIRS[@]}" \
             -release \
             -opensource \
             -confirm-license \
@@ -56,7 +67,6 @@ else
             -datadir $PREFIX \
             -L $PREFIX/lib \
             -I $PREFIX/include \
-            -I ${SRC_DIR}/openssl_hack/include \
             -release \
             -opensource \
             -confirm-license \
@@ -65,8 +75,6 @@ else
             -nomake tests \
             -verbose \
             -Wno-expansion-to-defined \
-            -openssl \
-            -openssl-linked \
             -opengl desktop
 fi
 
