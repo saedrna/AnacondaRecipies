54aa21af0399595e24d1496b14878a4c172835b3
han <huhan8807@gmail.com>
2019-05-24 01:08:52 +0800

    qt only fix gcc

--- qt5-declarative/build.sh
+++ qt5-declarative/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [[ $(uname) == "Linux" ]]; then
+    ln -s $BUILD_PREFIX/bin/${HOST}-g++ $BUILD_PREFIX/bin/g++ || true
+    ln -s $BUILD_PREFIX/bin/${HOST}-gcc $BUILD_PREFIX/bin/gcc || true
+    ln -s $BUILD_PREFIX/bin/${HOST}-gcc-ar $BUILD_PREFIX/bin/gcc-ar || true
+fi
 qmake CONFIG+=release
 make all -j$CPU_COUNT
 make install
