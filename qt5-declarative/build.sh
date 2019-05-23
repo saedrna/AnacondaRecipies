6bf3ef332b47d2ba1a6ef500254c2e3b071c6f07
han <huhan8807@gmail.com>
2019-05-24 01:20:13 +0800

    add ogl

--- qt5-declarative/build.sh
+++ qt5-declarative/build.sh
@@ -3,6 +3,9 @@ if [[ $(uname) == "Linux" ]]; then
     ln -s $BUILD_PREFIX/bin/${HOST}-g++ $BUILD_PREFIX/bin/g++ || true
     ln -s $BUILD_PREFIX/bin/${HOST}-gcc $BUILD_PREFIX/bin/gcc || true
     ln -s $BUILD_PREFIX/bin/${HOST}-gcc-ar $BUILD_PREFIX/bin/gcc-ar || true
+
+    declare -a INCDIRS
+    INCDIRS+=(INCLUDEPATH+=$BUILD_PREFIX/$HOST/sysroot/usr/include)
 fi
 qmake CONFIG+=release
 make all -j$CPU_COUNT
