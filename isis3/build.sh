b290fe1900f65e5d662e946c275797cc9b4f1bda
Han <huhan8807@gmail.com>
2019-05-24 15:06:48 +0800

    fix isis3

--- isis3/build.sh
+++ isis3/build.sh
@@ -1,6 +1,12 @@
 mkdir build
 cd build
 
+export AR=$(basename ${AR})
+export RANLIB=$(basename ${RANLIB})
+export STRIP=$(basename ${STRIP})
+export OBJDUMP=$(basename ${OBJDUMP})
+export CC=$(basename ${CC})
+export CXX=$(basename ${CXX})
 
 if [ "$(uname)" == "Darwin" ]; then
 cmake -GNinja \
@@ -13,12 +19,16 @@ cmake -GNinja \
       ../isis
 elif [ "$(uname)" == "Linux" ]; then
 cmake -GNinja \
-      -DJP2KFLAG=OFF \
-      -Dpybindings=OFF \
-      -DbuildTests=OFF \
+      -DCMAKE_SYSROOT=$BUILD_PREFIX/$HOST/sysroot \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations -Wno-misleading-indentation" \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-lrt ${CMAKE_SHARED_LINKER_FLAGS}" \
+      -DCMAKE_EXE_LINKER_FLAGS="-Wl,-lrt ${DCMAKE_EXE_LINKER_FLAGS}" \
+      -DJP2KFLAG=OFF \
+      -Dpybindings=OFF \
+      -DbuildTests=OFF \
       -DOPENCV_INCLUDE_DIR=$PREFIX/include/opencv4 \
       -DCHOLMOD_INCLUDE_DIR=$PREFIX/include/suitesparse \
       ../isis
