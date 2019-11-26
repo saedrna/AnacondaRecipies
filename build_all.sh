378b88269b79470009f669caadd805f567d0ad22
Han <huhan8807@gmail.com>
2019-11-26 22:31:29 +0800

    start build linux isis3

--- build_all.sh
+++ build_all.sh
@@ -4,52 +4,51 @@ conda config --set anaconda_upload yes
 
 conda-build purge
 
-conda-build alembic
-conda-build armadillo
-conda-build assimp
-conda-build boost-cpp
-conda-build capstone
-conda-build catch2
-conda-build cereal
-conda-build cgal-cpp
+# conda-build alembic
+# conda-build armadillo
+# conda-build assimp
+# conda-build boost-cpp
+# conda-build capstone
+# conda-build catch2
+# conda-build cereal
+# conda-build cgal-cpp
 conda-build flann
-conda-build geogram
+# conda-build geogram
 conda-build geographiclib
 conda-build geos
 conda-build gflags
 conda-build glfw3
 conda-build gmm
-conda-build gsl
+# conda-build gsl
 conda-build jama
-conda-build khronos-opencl-icd-loader
+# conda-build khronos-opencl-icd-loader
 conda-build laszip
 conda-build libgdal
 conda-build libtiff
-conda-build opengv
+# conda-build opengv
 conda-build qhull
 conda-build suitesparse
 conda-build superlu
 conda-build tbb
 conda-build tnt
-conda-build xerces-c
+# conda-build xerces-c
 
 conda-build glog
 
-
 conda-build ceres-solver
 conda-build geotiff
-conda-build openimageio
-conda-build viennacl
+# conda-build openimageio
+# conda-build viennacl
 
-conda-build coinutils
-conda-build osi
-conda-build clp
-conda-build openmvg
+# conda-build coinutils
+# conda-build osi
+# conda-build clp
+# conda-build openmvg
 
-conda-build open3d
-conda-build opencv
-conda-build openscenegraph
+# conda-build open3d
+# conda-build opencv
+# conda-build openscenegraph
 conda-build pcl
-conda-build pdal
+# conda-build pdal
 
 conda-build isis3
