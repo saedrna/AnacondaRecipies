c84c5807164c5146a1f9e624cd715cf8424fb063
Han <huhan8807@gmail.com>
2019-12-06 00:43:16 +0800

    libgdal do not build png

--- libgdal/build.sh
+++ libgdal/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --with-curl=no --with-xml2=no
+./configure --prefix=$PREFIX --with-curl=no --with-xml2=no --with-png=no
 
 make -j$CPU_COUNT
 make install
