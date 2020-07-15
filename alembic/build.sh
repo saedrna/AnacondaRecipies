8207e48fe85d96e250282881cfa9df6ce4e14cd3
Han <huhan8807@gmail.com>
2020-07-15 09:34:54 +0800

    [alembic] Fix linux build

--- alembic/build.sh
+++ alembic/build.sh
@@ -19,6 +19,13 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
       -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_SKIP_RPATH=ON \
       -DCMAKE_SYSROOT=$BUILD_PREFIX/$HOST/sysroot \
+      -DCMAKE_LIBRARY_PATH=$PREEFIX/lib \
+      -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+      -DALEMBIC_ILMBASE_HALF_LIB=$PREFIX/lib/libHalf.so \
+      -DALEMBIC_ILMBASE_IEXMATH_LIB=$PREFIX/lib/libIexMath.so \
+      -DALEMBIC_ILMBASE_IEX_LIB=$PREFIX/lib/libIex.so \
+      -DALEMBIC_ILMBASE_ILMTHREAD_LIB=$PREFIX/lib/libIlmThread.so \
+      -DALEMBIC_ILMBASE_IMATH_LIB=$PREFIX/lib/libImath.so \
       -DUSE_TESTS=OFF \
       -DALEMBIC_SHARED_LIBS=ON \
       ../
