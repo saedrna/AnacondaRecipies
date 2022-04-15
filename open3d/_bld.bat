f5082b3a5269a25d4e655633da8011db51cbedb4
Han Hu <huhan8807@gmail.com>
2022-04-15 15:30:19 +0800

    open3d 最新版加入了 devel 库，虽然完成了 cmake 编译，但是直接用现有的

---
+++ open3d/_bld.bat
@@ -0,0 +1,46 @@
+@echo ON
+setlocal enabledelayedexpansion
+
+set HTTP_PROXY=http://127.0.0.1:10809
+set HTTPS_PROXY=http://127.0.0.1:10809
+
+set UNIX_PREFIX=%PREFIX:\=/%
+set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
+set UNIX_LIBRARY_INC=%LIBRARY_INC:\=/%
+set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
+set UNIX_SP_DIR=%SP_DIR:\=/%
+set UNIX_SRC_DIR=%SRC_DIR:\=/%
+
+mkdir build
+cd build
+
+cmake -LAH -G "Visual Studio 17 2022" -A x64 ^
+    -DCMAKE_BUILD_TYPE="Release" ^
+    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%UNIX_LIBRARY_PREFIX% ^
+    -DBUILD_CPP_EXAMPLES=OFF ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_PYTHON_MODULE=OFF ^
+    -DBUILD_PYBIND11=OFF ^
+    -DENABLE_JUPYTER=OFF ^
+    -DBUILD_GUI=OFF ^
+    -DENABLE_HEADLESS_RENDERING=OFF ^
+    -DSTATIC_WINDOWS_RUNTIME=OFF ^
+    -DUSE_BLAS=OFF ^
+    -DUSE_SYSTEM_EIGEN3=ON ^
+    -DUSE_SYSTEM_FLANN=ON ^
+    -DUSE_SYSTEM_JPEG=ON ^
+    -DUSE_SYSTEM_PNG=ON ^
+    -DUSE_SYSTEM_GLEW=ON ^
+    -DUSE_SYSTEM_GLFW=ON ^
+    -DUSE_SYSTEM_PYBIND11=ON ^
+    ..
+
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
+
+exit 0
--- open3d/bld.bat
+++ open3d/bld.bat
@@ -1,9 +1,6 @@
 @echo ON
 setlocal enabledelayedexpansion
 
-set HTTP_PROXY=http://127.0.0.1:10809
-set HTTPS_PROXY=http://127.0.0.1:10809
-
 set UNIX_PREFIX=%PREFIX:\=/%
 set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
 set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
@@ -12,35 +9,11 @@ set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
 set UNIX_SP_DIR=%SP_DIR:\=/%
 set UNIX_SRC_DIR=%SRC_DIR:\=/%
 
-mkdir build
-cd build
+xcopy /S/E/I "%SRC_DIR%/lib" "%UNIX_LIBRARY_PREFIX%/lib/"
+xcopy /S/E/I "%SRC_DIR%/bin" "%UNIX_LIBRARY_PREFIX%/bin/"
+xcopy /S/E/I "%SRC_DIR%/CMake" "%UNIX_LIBRARY_PREFIX%/cmake/"
+xcopy /S/E/I "%SRC_DIR%/include" "%UNIX_LIBRARY_PREFIX%/include/"
 
-cmake -LAH -G "Visual Studio 16 2019" -A x64 ^
-    -DCMAKE_BUILD_TYPE="Release" ^
-    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX% ^
-    -DCMAKE_PREFIX_PATH=%UNIX_LIBRARY_PREFIX% ^
-    -DBUILD_CPP_EXAMPLES=OFF ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -DBUILD_EXAMPLES=OFF ^
-    -DBUILD_PYTHON_MODULE=OFF ^
-    -DBUILD_PYBIND11=OFF ^
-    -DENABLE_JUPYTER=OFF ^
-    -DBUILD_GUI=OFF ^
-    -DENABLE_HEADLESS_RENDERING=OFF ^
-    -DSTATIC_WINDOWS_RUNTIME=OFF ^
-    -DUSE_BLAS=OFF ^
-    -DUSE_SYSTEM_EIGEN3=OFF ^
-    -DUSE_SYSTEM_FLANN=ON ^
-    -DUSE_SYSTEM_JPEG=ON ^
-    -DUSE_SYSTEM_PNG=ON ^
-    -DUSE_SYSTEM_GLEW=ON ^
-    -DUSE_SYSTEM_GLFW=ON ^
-    -DUSE_SYSTEM_PYBIND11=ON ^
-    ..
-
-if errorlevel 1 exit 1
-
-cmake --build . --target install --config Release
 if errorlevel 1 exit 1
 
 exit 0
